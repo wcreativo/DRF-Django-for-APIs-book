cff71d397de551fa54f920f6d3dbb0a6480aec4d
Wilbertson <w.kreativo@gmail.com>
2021-06-10 07:51:58 -0500

    Adding API permissions

--- BlogAPI/posts/views.py
+++ BlogAPI/posts/views.py
@@ -1,6 +1,7 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Post
 from .serializers import PostSerializer
+from .permissions import IsAuthorOrReadOnly
 
 
 class PostList(generics.ListCreateAPIView):
@@ -9,5 +10,6 @@ class PostList(generics.ListCreateAPIView):
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
